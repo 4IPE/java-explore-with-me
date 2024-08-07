3d6e21c14d2abf0167b0b23cf710461c16f6ee4e
daniilbykov <all0b0y@yandex.ru>
2024-08-07 20:49:17 +0300

    <fix>Исправление ошибок

--- ewm-service/src/main/java/service/admin/service/CompilationsServiceImpl.java
+++ ewm-service/src/main/java/service/admin/service/CompilationsServiceImpl.java
@@ -2,7 +2,6 @@ package service.admin.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import service.dto.compilations.CompilationsInDto;
 import service.dto.compilations.CompilationsOutDto;
 import service.exception.model.NotFound;
